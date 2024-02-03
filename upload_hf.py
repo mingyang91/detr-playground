545fdb1a361f6a402910c9305a04123e10ccaebf
mingyang91 <my@famer.me>
2024-02-03 18:20:37 +0800

    format code

--- upload_hf.py
+++ upload_hf.py
@@ -1,7 +1,7 @@
 from segformer_trainer import SegformerFineTuner, id2label
 from transformers import SegformerImageProcessor
 processor = SegformerImageProcessor.from_pretrained('nvidia/mit-b5')
-model = SegformerFineTuner.load_from_checkpoint("./lightning_logs/version_28/checkpoints/epoch=125-step=126.ckpt", num_labels=len(id2label.keys()))
+model = SegformerFineTuner.load_from_checkpoint("./lightning_logs/version_29/checkpoints/epoch=236-step=237.ckpt", num_labels=len(id2label.keys()))
 
 
 model.model.push_to_hub("mingyang91/segformer-for-surveillance")
